02e1e578b9af1792210f7ab01802a028cce3b784
Patrick Mashaba <patrickmashaba@gmail.com>
2024-05-11 07:45:57 +0200

    Building the ui using @nuxt/ui library

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="board-wrapper">
+    <main class="board">
+      <UContainer
+        v-for="column in boardStore.board.columns"
+        :key="column.name"
+        class="column"
+      >
+        <h2 class="mb-4">{{ column.name }}</h2>
+        <ul>
+          <li v-for="task in column.tasks" :key="task.id">
+            <UCard class="mb-4">
+              <strong>{{ task.name }}</strong>
+              <p>{{ task.description }}</p>
+            </UCard>
+          </li>
+        </ul>
+      </UContainer>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { useBoardStore } from '~/stores/boardStore';
+
+const boardStore = useBoardStore();
+</script>
+
